01bab1209720708bbb321f794c6d3a503bebae71
josedoce <josesamya@gmail.com>
2023-01-19 15:04:44 -0300

    second day: web app

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -6,7 +6,14 @@ module.exports = {
   ],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#09090A'
+      },
+      gridTemplateRows: {
+        7: 'repeat(7, minmax(0, 1fr))'
+      }
+    },
   },
   plugins: [],
 }
